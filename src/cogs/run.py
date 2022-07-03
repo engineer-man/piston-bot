2a05530f4601b89f366cd90e8b89be1e7f06b4de
brtwrst <6873862+brtwrst@users.noreply.github.com>
2022-07-03 07:17:39 +0000

    add debug command

--- src/cogs/run.py
+++ src/cogs/run.py
@@ -7,7 +7,7 @@ Commands:
 """
 # pylint: disable=E0402
 import json
-import re
+import re, sys
 from dataclasses import dataclass
 from discord import Embed, Message, errors as discord_errors
 from discord.ext import commands, tasks
@@ -23,6 +23,25 @@ class RunIO:
     input: Message
     output: Message
 
+def get_size(obj, seen=None):
+    """Recursively finds size of objects"""
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum([get_size(v, seen) for v in obj.values()])
+        size += sum([get_size(k, seen) for k in obj.keys()])
+    elif hasattr(obj, '__dict__'):
+        size += get_size(obj.__dict__, seen)
+    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum([get_size(i, seen) for i in obj])
+    return size
 
 class Run(commands.Cog, name='CodeExecution'):
     def __init__(self, client):
@@ -325,6 +344,14 @@ class Run(commands.Cog, name='CodeExecution'):
                 # Message no longer exists in discord
                 del self.run_IO_store[ctx.author.id]
             return
+
+    @commands.command(hidden=True)
+    async def size(self, ctx):
+        if ctx.author.id != 98488345952256000:
+            return False
+        await ctx.send(
+            f'```\nIO Cache {len(self.run_IO_store)} / {get_size(self.run_IO_store) // 1000} kb'
+            f'\nMessage Cache {len(self.client.cached_messages)} / {get_size(self.client.cached_messages)} kb\n```')
 
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
